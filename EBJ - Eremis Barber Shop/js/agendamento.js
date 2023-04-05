d4cb14ee415dd2fec480bcd8fe9741d1d657c6f7
Mr. Kuro <queirozanderson467@gmail.com>
2023-04-05 03:56:49 -0300

    EBJ - corrigindo bugs e melhorando design, pt2

--- EBJ - Eremis Barber Shop/js/agendamento.js
+++ EBJ - Eremis Barber Shop/js/agendamento.js
@@ -1,103 +1,67 @@
+// Pegando os valores que serão adicionados ao input e atribuindo os mesmos a variaveis
+let peso = document.querySelector("#peso");
+let altura = document.querySelector("#altura");
+// Deixando o foco no peso
+peso.focus();
 
-function desenhaQuadrado(x, y, tamanho, cor) {
+//Função para realizar o calculo do imc
+function getIMC() {
+  // Condicional para saber se os valores adicionados nos campos são valores validos.
+  if (
+    altura.value < 2.5 &&
+    peso.value < 300 &&
+    altura.value > 0 &&
+    peso.value > 0
+  ) {
+    // Calculando o imc e atribuindo - o a uma variavel
+    let imc = peso.value / (altura.value * altura.value);
 
-    pincel.fillStyle = cor;
-    pincel.fillRect(x, y, tamanho, tamanho)
-    pincel.fill();
+    //Fixando as casas decimais em somente duas
+    imc = imc.toFixed(2);
+
+    // Limpa os valores digitados para uma nova consulta
+    peso.value = "";
+    altura.value = "";
+
+    // Adicionando um texto a um elemento vazio para exibir o resultado
+    document.querySelector("#result").innerText =
+      "Sua situação imc é de " + imc;
+
+    checkIMC(imc);
+
+    // Funcão que analisa os valores do imc e os classifica segundo cada condição fisica
+  } else {
+    document.querySelector("#result").innerText =
+      "Os valores digitados são invalidos!";
+
+    document.querySelector("#info").innerText =
+      `Por quê tem uma cauculadora de IMC na página de agendamento?
+      Porque o Site é Meu, e faço dele o que quizer :3!`;
+
+    peso.value = "";
+    altura.value = "";
+  }
 }
 
-function desenhaCirculo(x, y, raio, cor) {
-
-    pincel.fillStyle = cor; // o padrão é blue
-    pincel.beginPath();
-    pincel.arc(x, y, raio, 0, 2 * 3.14);
-    pincel.fill();
-
+function checkIMC(imc) {
+  let text = "Você esta classificado como ";
+  if (imc < 18.5) {
+    document.querySelector("#info").innerText = text + "magresa";
+  } else if (imc >= 18.5 && imc <= 24.9) {
+    document.querySelector("#info").innerText = text + "Normal";
+  } else if (imc >= 25.0 && imc <= 29.9) {
+    document.querySelector("#info").innerText = text + "Sobrepeso";
+  } else if (imc >= 30.0 && imc <= 34.9) {
+    document.querySelector("#info").innerText = text + "Obesidade grau I";
+  } else if (imc >= 35.0 && imc <= 39.9) {
+    document.querySelector("#info").innerText = text + "Obesidade grau II";
+  } else if (imc > 40.0) {
+    document.querySelector("#info").innerText = text + "Obesidade grau III";
+  }
 }
 
-function desenhaPaletaDeCores() {
+// Selecionando o botão
+// let button = document.querySelector("button");
 
-    desenhaQuadrado(xVermelho, yQuadrados, tamanhoQuadrados, 'red');
-    desenhaQuadrado(xVerde, yQuadrados, tamanhoQuadrados, 'green');
-    desenhaQuadrado(xAzul, yQuadrados, tamanhoQuadrados, 'blue');
-
-}
-
-function lidaComMovimentoDoMouse(evento) {
-
-    var x = evento.pageX - tela.offsetLeft;
-    var y = evento.pageY - tela.offsetTop;
-
-    // usando a nova função 
-    if(desenha && podeDesenharNaArea(x,y)) {
-
-        desenhaCirculo(x, y, 5, corAtual);
-    }
-}
-
-function habilitaDesenhar() {
-
-    desenha = true;
-}
-
-function desabilitaDesenhar() {
-
-    desenha = false;
-}
-
-function podeDesenharNaArea(x, y) {
-
-     if(x >= 0 && x < 3*tamanhoQuadrados && y >= 0 && y < tamanhoQuadrados) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-function selecionaCor(evento) {
-
-    var x = evento.pageX - tela.offsetLeft;
-    var y = evento.pageY - tela.offsetTop;
-
-    // cada condição muda a variável `corAtual`
-
-    // começamos pela condição do y que é igual em todos os casos
-    if (y > yQuadrados && y < yQuadrados + tamanhoQuadrados) {
-
-        if (x > xVermelho && x < xVermelho + tamanhoQuadrados) {
-
-            corAtual = 'red';
-
-        } else if (x > xVerde && x < xVerde + tamanhoQuadrados) {
-
-            corAtual = 'green';
-
-        } else if (x > xAzul && x < xAzul + tamanhoQuadrados) {
-
-            corAtual = 'blue';
-        }
-    }
-}
-
-var tela = document.querySelector('canvas');
-var pincel = tela.getContext('2d');
-pincel.fillStyle = 'lightgray';
-pincel.fillRect(0, 0, 600, 400);
-
-var desenha = false;
-var corAtual = 'blue';
-var xVermelho = 0;
-var xVerde  = 50;
-var xAzul = 100;
-var yQuadrados = 0;
-var tamanhoQuadrados = 50;
-
-desenhaPaletaDeCores();
-
-tela.onmousemove = lidaComMovimentoDoMouse;
-
-tela.onmousedown = habilitaDesenhar;
-
-tela.onmouseup = desabilitaDesenhar;
-
-tela.onclick = selecionaCor;
+//Realizando a chamada do botão e associando o mesmo com o onclick, recebendo a função getIMC
+// button.onclick = getIMC;
