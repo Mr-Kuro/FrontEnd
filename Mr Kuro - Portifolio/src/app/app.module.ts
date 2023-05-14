3dfdad16cf935700505c7860e9d93bec0958674e
Mr. Kuro <queirozanderson467@gmail.com>
2023-05-14 02:45:58 -0300

    Mr_Kuro portifólio

--- Mr Kuro - Portifolio/src/app/app.module.ts
+++ Mr Kuro - Portifolio/src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/sharing/header/header.component';
+import { FooterComponent } from './pages/sharing/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
